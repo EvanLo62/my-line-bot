0eafa9284be7ce03a387fb53b9afa30038567bfc
Evan Lo <monstercontcer1@gmail.com>
2023-05-04 20:07:10 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -71,4 +71,4 @@ def handle_message(event):
         TextSendMessage(text=output_text))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
