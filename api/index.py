7fbf11004049ed5fce6bf14b29981420025e45a3
Evan Lo <monstercontcer1@gmail.com>
2023-05-04 16:16:47 +0800

    update

--- api/index.py
+++ api/index.py
@@ -1,3 +1,6 @@
+import openai
+import os
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -12,12 +15,23 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('UNqrgdVFsi5OPdNsyy6H3zp5tkJqPquCDcS596bgU1tggL9n60QUam8ZQQQqdgAkvPqZpKugnAqN6N5Id11xiMRq7ynRmg3n/AdYpqpw3w2bGx8eNo03kqS6iN0ZIzxRgmEH4Zasny2lfQyY4TZxUAdB04t89/1O/w1cDnyilFU=')
-webhook_handler = WebhookHandler('2c61617dd7275afc7127ec4c280b02a5')
+line_bot_api = LineBotApi('WnOAh83Ss4qSMw/6INKsbJnAdHS7Xe8T7qMRJ4bPwrzIEzkRezQDM2ow3KFjsQYqp2YHyvqOAaGdhFllhKLqjkEcRK8Kl/SBjqBGM+9SwDbaeQNahdjrp0TpsK6S3U32kwsr/4YVxTnH9O4nAUFySwdB04t89/1O/w1cDnyilFU=')
+webhook_handler = WebhookHandler('4a02436c986b12d55a812b590e515f96')
+
+# 設定 OpenAI API 密鑰
+openai.api_key = os.environ["OPENAI_API_KEY"]
+# openai.api_key = "OPENAI_API_KEY"
+
+# 設定 GPT-3.5 模型的檢索引擎
+model_engine = "text-davinci-003"
+
+# 設定生成的文本長度
+output_length = 300
+
 
 @app.route("/")
 def home():
-    return "Line Bot API Server is Running"
+    return "LINE BOT API Server is running."
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,9 +54,20 @@ def callback():
 
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    # 使用 GPT-3.5 模型生成文本
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=event.message.text,
+        max_tokens=output_length,
+    )
+
+    # 取得生成的文本
+    output_text = response.choices[0].text.strip()
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=output_text))
 
 
 if __name__ == "__main__":
