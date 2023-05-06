758eaf20442a3f399b43e6fba799c1c9f5a94853
Evan Lo <monstercontcer1@gmail.com>
2023-05-06 21:34:14 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -23,10 +23,12 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 # openai.api_key = "OPENAI_API_KEY"
 
 # 設定 GPT-3.5 模型的檢索引擎
-model_engine = "text-davinci-002"
+model_engine = "text-davinci-003"
 
 # 設定生成的文本長度
-output_length = 50
+output_length = 200
+
+temp = 0.5
 
 
 @app.route("/")
@@ -59,11 +61,12 @@ def handle_message(event):
     response = openai.Completion.create(
         engine=model_engine,
         prompt=event.message.text,
-        max_tokens = output_length
+        max_tokens = output_length,
+        temperature = temp
     )
 
     # 取得生成的文本
-    output_text = response.choices[0].text.strip()
+    output_text = response.choices[0].text
 
     line_bot_api.reply_message(
         event.reply_token,
