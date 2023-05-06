69936dc6186673a3a839d3470cd2a43a479469c5
Evan Lo <monstercontcer1@gmail.com>
2023-05-06 21:43:38 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -18,16 +18,27 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('UNqrgdVFsi5OPdNsyy6H3zp5tkJqPquCDcS596bgU1tggL9n60QUam8ZQQQqdgAkvPqZpKugnAqN6N5Id11xiMRq7ynRmg3n/AdYpqpw3w2bGx8eNo03kqS6iN0ZIzxRgmEH4Zasny2lfQyY4TZxUAdB04t89/1O/w1cDnyilFU=')
 webhook_handler = WebhookHandler('2c61617dd7275afc7127ec4c280b02a5')
 
-# 設定 OpenAI API 密鑰
-openai.api_key = os.environ["OPENAI_API_KEY"]
-# openai.api_key = "OPENAI_API_KEY"
+def chatgpt(input):
+    # 設定 OpenAI API 密鑰
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+    # openai.api_key = "OPENAI_API_KEY"
 
-# 設定 GPT-3.5 模型的檢索引擎
-model_engine = "text-davinci-003"
+    # 設定 GPT-3.5 模型的檢索引擎
+    model_engine = "text-davinci-003"
 
-# 設定生成的文本長度
-output_length = 200
+    # 設定生成的文本長度
+    output_length = 200
 
+    input_text = input
+
+        # 使用 GPT-3.5 模型生成文本
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=input_text,
+        max_tokens = output_length,
+    )
+
+    return response.choices[0].text
 
 @app.route("/")
 def home():
@@ -55,19 +66,9 @@ def callback():
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    # 使用 GPT-3.5 模型生成文本
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=event.message.text,
-        max_tokens = output_length,
-    )
-
-    # 取得生成的文本
-    output_text = response.choices[0].text
-
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=output_text))
+        TextSendMessage(chatgpt(event.message.text)))
 
 if __name__ == "__main__":
     app.run()
